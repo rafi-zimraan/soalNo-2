517f3cd3357c5926c5ebc1c303d58650f6623d7a
rafi-zimraan <raffizimraan27@gmail.com>
2023-02-16 17:21:30 +0700

    first commit

---
+++ soal 2.js
@@ -0,0 +1,5 @@
+// SOAL NO. 2
+let lisDesa =['argodadi,sedayu','argomulyo,sedayu','argorejo,sedayu','argosari','bangunjiwo,kasihan','banguntapan,banguntapan','bantul,bantul','baturetno,banguntapan','bawuran,pleret','canden,jetis','caturharjo,pandak','cetan srigandi']
+    for (let i = 0; +i < lisDesa.length; i++) {
+    console.log( ` hasil dari soal 2 : `+ i + " " + lisDesa[i]);
+}
